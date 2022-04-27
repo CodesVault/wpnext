6c8d5ded3443d3971d9cdac742309c7168d9fb2f
AbmSourav <keramotul.islam@gmail.com>
2022-04-27 14:33:22 +0600

    main layout component added

--- pages/index.js
+++ pages/index.js
@@ -1,35 +1,30 @@
-import Head from 'next/head'
 // import Image from 'next/image'
 import Link from 'next/link'
+import Layout from '../src/components/layouts/layout'
 import styles from '../styles/post.module.css'
 
 export default function Home({ posts }) {
     // console.log('posts', posts)
     return (
-        <div className={styles.container}>
-            <Head>
-                <title>WP NextJS</title>
-                <meta name="description" content="Template for creating NextJS app for Headless WordPress" />
-                <link rel="icon" href="/favicon.ico" />
-            </Head>
-
-            <div className={styles.wrapper}>
-                {posts.nodes.map(post => {
-                    const date = new Date(post.date).toLocaleString(undefined, {year: 'numeric', month: '2-digit', day: '2-digit'})
-                    
-                    return(
-                        <div className={styles.post} key={post.slug}>
-                            <h2 className={styles.title}>
-                                <Link href={`/posts/${post.slug}`}>{post.title}</Link>
-                            </h2>
-                            <div className={styles.date}>{date}</div>
-                            <div dangerouslySetInnerHTML={{__html: post.excerpt}}></div>
-                        </div>
-                    )
-                })}
+        <Layout>
+            <div className={styles.container}>
+                <div className={styles.wrapper}>
+                    {posts.nodes.map(post => {
+                        const date = new Date(post.date).toLocaleString(undefined, {year: 'numeric', month: '2-digit', day: '2-digit'})
+                        
+                        return(
+                            <div className={styles.post} key={post.slug}>
+                                <h2 className={styles.title}>
+                                    <Link href={`/posts/${post.slug}`}>{post.title}</Link>
+                                </h2>
+                                <div className={styles.date}>{date}</div>
+                                <div dangerouslySetInnerHTML={{__html: post.excerpt}}></div>
+                            </div>
+                        )
+                    })}
+                </div>
             </div>
-
-        </div>
+        </Layout>
     )
 }
 
